e8e08c3c36f1f231e24b22df967c25bd6989cb59
Apoorv Malik <apoorv258@gmail.com>
2023-02-12 04:39:11 -0800

    Merge branch 'frontend' of https://github.com/1998apoorvmalik/linear-fold into frontend
    Conflicts:
    	templates/linear_sankoff.html
    	templates/showResult_linearsankoff.html

--- check_request.sh
+++
@@ -1,7 +0,0 @@
-#ls -lt  static/pairingRes/ | tail -n +2 | awk '{print $6" "$7","$8}' | sort | uniq -c
-echo "LinearFold"
-grep "LF file" usrLog.txt | awk -F"[][ ]" '{print $3" "$4","$6}' | sort | uniq -c
-echo "LinearPartition"
-grep "LP file" usrLog.txt | awk -F"[][ ]" '{print $3" "$4","$6}' | sort | uniq -c
-echo "Linear_all"
-grep " file" usrLog.txt | awk -F"[][ ]" '{print $3" "$4","$6}' | sort -n | uniq -c
