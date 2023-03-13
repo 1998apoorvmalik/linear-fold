d5ba62183aaa780e6d90611c808f320bd58b46db
Apoorv Malik <apoorv258@gmail.com>
2023-03-13 01:45:21 -0700

    Added input validation

--- static/js/interface.js
+++ static/js/interface.js
@@ -1,3 +1,93 @@
+let pageName = window.location.href.split("/").pop();
+let pageElement = document.getElementById(pageName);
+
+// check is page exist
+if (pageElement) {
+  // add class active to page
+  pageElement.classList.add("active");
+} else {
+  // add class active to home page
+  document.getElementById("linearfold").classList.add("active");
+}
+
+function fileUploadListener() {
+  const fileUpload = document.getElementById("SeqFile");
+  const fileContents = document.getElementById("seqInput");
+
+  if (fileUpload) {
+    fileUpload.addEventListener("change", function () {
+      const file = fileUpload.files[0];
+      const reader = new FileReader();
+      reader.addEventListener("load", function () {
+        if (fileContents) {
+          fileContents.value = reader.result;
+        }
+      });
+      reader.readAsText(file);
+    });
+  }
+}
+
+function validateInput(id, min, max) {
+  let input = document.getElementById(id);
+  let value = input.value;
+
+  if (value < min || value > max || isNaN(value)) {
+    input.classList.add("is-invalid");
+    input.classList.remove("is-valid");
+    return false;
+  } else {
+    input.classList.remove("is-invalid");
+    input.classList.add("is-valid");
+    // remove red border
+    input.style.border = "1px solid #ced4da";
+    return true;
+  }
+}
+
+function makeFornaUrl(baseUrl, sequence, structure) {
+  let url = baseUrl + "?id=fasta&file=%3Eheader%5Cn" + sequence + "%5Cn" + structure;
+  return url;
+}
+
+// ------------------------------------------------------ Linear Sankoff Specific Interface ------------------------------------------------------------
+const LS_SAMPLES = [
+  ">tdbD00005111.dotbracket\nGCCGUGAUAGCUCAGUUGGGAGAGCGUCAGACUGAAGAUCUGAAGGUCCCUGGUUCGAUCCCUGGUCACGGCA\n>tdbD00001770.dotbracket\nGUCAGGAUGGCCGAGUGGUCUAAGGCGCUGCGUUCAGGUCGCAGUCUACUCUGUAGGCGUGGGUUCGAAUCCCACUUCUGACA",
+  ">seq1\nGUUCCCGUCUAUUGCAAGACACCAGUUACUGAAUGUGUUCUGUUGGAAAGAGAACAUAUUGAAGUAAUAAUGAACUGCAAAC\n>seq2\nGCUGUCUGGCGCGGGCGGGAGUCGUCCUUGGUGGCCUUCCGUAGGGGAGGCUUCCGGGUCGUUGAAUGCGGCGUACCGCUUC",
+  ">seq1\nGUUCCCGUCUAUUGCAAGACACCAGU\n>seq2\nGCUGUCUGGCGCGGGCGGGAGUCGU",
+  ">B00650 118\nGCCUGGUGGCUAUGGCGAGGAGGCCUAACCCGAUCCCAUCCCGAACUCGGCCGUUAAACUCCUCAGUGCCGAUGGUACUAUGUCUUAAGACCUGGGAGAGUAGGUCGUCGCCAGGCCU\n>B01704 115\nCCUGGCGGCGAUAGUGCGGUGGACCCACCUGAGACCAUACCGAACUCAGAAGUGAAACGCUGUAAUGCCGAUGGUAGUGUGGGGUUUCCCCAUGUGAGAGUAGGGCACCGCCGGG",
+];
+
+if (pageName == "linearsankoff") {
+  document.getElementById("footer").classList.add("disabled");
+}
+
+// check if page containes an id 'result'
+if (document.getElementById("result-page")) {
+  // disable id cluster map
+  document.getElementById("footer").classList.add("disabled");
+}
+
+function loadLinearSankoffSample(index) {
+  if (index < LS_SAMPLES.length) {
+    document.getElementById("seqInput").value = LS_SAMPLES[index];
+  } else {
+    document.getElementById("seqInput").value = index;
+  }
+}
+// -----------------------------------------------------------------------------------------------------------------------------------------------------
+
+// .
+// .
+// .
+// .
+// .
+
+// --------------------------------------------********** WARNING ********ß**-----------------------------------------------
+// --------------------------------------------* TOUCH AT DEVELOPER'S RISK *-----------------------------------------------
+// --------------------------------------------*--- Might Break Things ----*-----------------------------------------------
+// --------------------------------------------******* Kaibo's code ********----------------------------------------------------
+
 if ($("#seqInput").text() == "") {
   $("#seqInput").css("color", "lightgrey");
   $("#submit").prop("disabled", true);
@@ -80,8 +170,3 @@ window.onload = function (e) {
   $("#jobID").val(Date.now().toString(36) + Math.random().toString(36).substring(7));
   // console.log($('#jobID').val());
 };
-
-function makeFornaUrl(baseUrl, sequence, structure) {
-  let url = baseUrl + "?id=fasta&file=%3Eheader%5Cn" + sequence + "%5Cn" + structure;
-  return url;
-}
